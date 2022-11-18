4e921c388313f6fd0807ad6a5b67795e6e2de5c8
Temirov Erali <eralitemirov129@gmail.com>
2022-11-18 18:59:26 +0300

    m

---
+++ main.js
@@ -0,0 +1,16 @@
+alert("Saytimizga xush kelibsiz");
+var ismingiz = prompt("Ismingizni kiriting? ");
+console.log("Ismi:" + ismingiz);
+var familyangiz = prompt("Familyangizni kiriting? ");
+console.log("Familya:" + familyangiz);
+var yoshingiz = prompt("Yoshingizni kiriting? ");
+console.log("Yosh:" + yoshingiz);
+var manzil = prompt("Yashash joyingizni kiriting? ");
+console.log("Manzil:" + manzil);
+var malumot = prompt("O'zingiz hqida malumotni kiriting? ");
+console.log("Malumot:" + malumot);
+var parol = prompt("Parolingizni kiriting? ");
+console.log("Parol:" + parol);
+document
+.querySelector("button")
+.addEventListener("click", ()=> alert("Saytimizdan foydalanganiz uchun minnatdormiz"));
